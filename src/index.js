34d4f223f31a7e3c8705f674b5b9a45b8870e1b3
DESKTOP-OEQ7TM8\Argam <argam.tik@gmail.com>
2020-07-02 16:23:25 +0400

    Added base name to Router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 
 const App = () => {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div>
                 <Header />
                 <Switch>
